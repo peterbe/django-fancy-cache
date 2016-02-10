ec04903bdde815a8fc2a32624c2b611de6818cc2
Peter Bengtsson <mail@peterbe.com>
2016-02-10 08:34:41 -0500

    default template settings in tests

--- fancy_tests/tests/settings.py
+++ fancy_tests/tests/settings.py
@@ -29,3 +29,20 @@ INSTALLED_APPS = [
 
 
 ROOT_URLCONF = 'fancy_tests.tests.urls'
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
