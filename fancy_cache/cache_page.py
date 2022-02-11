9fba5c36ce2b42bca493ddcc34e2ac4c575a0500
Ian Campbell <ipcampbell@gmail.com>
2022-02-11 10:03:57 -0500

    Update fancy_cache/middleware.py to match the latest Django middleware file. See https://github.com/django/django/blob/main/django/middleware/cache.py Fixes #14.

--- fancy_cache/cache_page.py
+++ fancy_cache/cache_page.py
@@ -2,11 +2,11 @@ import typing
 
 from django.utils.decorators import decorator_from_middleware_with_args
 
-from .middleware import CacheMiddleware
+from .middleware import FancyCacheMiddleware
 
 
 def cache_page(timeout: float, *args, **kwargs) -> typing.Callable:
     cache_alias = kwargs.pop("cache", None)
-    return decorator_from_middleware_with_args(CacheMiddleware)(
+    return decorator_from_middleware_with_args(FancyCacheMiddleware)(
         timeout, *args, cache_alias=cache_alias, **kwargs
     )
