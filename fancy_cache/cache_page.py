6b54918b2aec8b839ff00fd6c5c0059b9bb9fc39
Ian Campbell <ipcampbell@gmail.com>
2022-03-10 10:01:14 -0500

    Make timeout an optional parameter for cache_page. Fixes #66.

--- fancy_cache/cache_page.py
+++ fancy_cache/cache_page.py
@@ -5,7 +5,9 @@ from django.utils.decorators import decorator_from_middleware_with_args
 from .middleware import FancyCacheMiddleware
 
 
-def cache_page(timeout: float, *args, **kwargs) -> typing.Callable:
+def cache_page(
+    timeout: typing.Optional[float], *args, **kwargs
+) -> typing.Callable:
     cache_alias = kwargs.pop("cache", None)
     return decorator_from_middleware_with_args(FancyCacheMiddleware)(
         timeout, *args, cache_alias=cache_alias, **kwargs
