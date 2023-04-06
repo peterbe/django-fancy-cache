51c1ce6fd6b212319b23beb29d2957450496dd27
Ian Campbell <ipcampbell@gmail.com>
2023-04-06 16:28:50 -0400

    Make sure the key for REMEMBERED_URLS is correct when using Memcached check-and-set.

--- fancy_tests/tests/test_views.py
+++ fancy_tests/tests/test_views.py
@@ -323,29 +323,3 @@ class TestViews(unittest.TestCase):
         # Make sure clearing the dummy cache doesn't raise an error,
         # even though it should do nothing.
         caches["dummy_backend"].clear()
-
-    @mock.patch("fancy_cache.middleware.USE_MEMCACHED_CAS", True)
-    def test_cache_dummy_backend_with_memcached_check_and_set(self):
-        """
-        Test that the Dummy cache backend works as expected by not caching
-        when FANCY_USE_MEMCACHED_CHECK_AND_SET is set to True.
-        """
-        request = self.factory.get("/anything")
-
-        response = views.home8(request)
-        eq_(response.status_code, 200)
-        ok_(re.findall("Random:\w+", response.content.decode("utf8")))
-        random_string_1 = re.findall(
-            "Random:(\w+)", response.content.decode("utf8")
-        )[0]
-
-        response = views.home8(request)
-        eq_(response.status_code, 200)
-        random_string_2 = re.findall(
-            "Random:(\w+)", response.content.decode("utf8")
-        )[0]
-        ok_(random_string_1 != random_string_2)
-
-        # Make sure clearing the dummy cache doesn't raise an error,
-        # even though it should do nothing.
-        caches["dummy_backend"].clear()
